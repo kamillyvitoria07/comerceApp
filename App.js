235b8f1f520d8093266e96b25adfc89a0839f3c8
Kamilly <otakubrava.kamilly@gmail.com>
2024-06-10 15:26:16 -0300

    comerceApp

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import "react-native-gesture-handler";
+import Shoes from './src/components/Shoes/index.js';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { AppLoading } from "expo";
+import { useFonts, Anton_400Regular } from "@expo-google-fonts/anton";
+import Routes from "./src/router";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ let [fontsLoaded] = useFonts({
+ Anton_400Regular,
+ });
+ // if (!fontsLoaded) {
+ // return <AppLoading />;
+ // }
+ return (
+ <>
+ <StatusBar style="light" backgroundColor="#000" translucent={true} />
+ <Routes />
+ </>
+ );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
